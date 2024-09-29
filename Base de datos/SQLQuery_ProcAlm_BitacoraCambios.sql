f8e7ae25d12ce9f9cc4b66a3cb1fef0256ddbf2c
Lautaro Rojas <Lautaro.Rojas02@live.com>
2024-09-29 20:01:43 -0300

    Guardar los procedimientos almacenados de bitacora cambios

--- Base de datos/SQLQuery_ProcAlm_BitacoraCambios.sql
+++ Base de datos/SQLQuery_ProcAlm_BitacoraCambios.sql
@@ -2,22 +2,17 @@ CREATE PROC BITACORA_CAMBIOS_ENTRADA_INSERTAR
 @idusuarioquemodifica int, @fechadecambio DateTime, @activo bit, @b_identrada int, @b_idusuario int, @b_idfuncion int, @b_idsala int, @b_butaca varchar(50), @b_precio float, @b_fechadereserva DateTime, @b_estado varchar(50), @b_digitohorizontal varchar(70)
 AS
 BEGIN
-	UPDATE BITACORACAMBIOS_ENTRADA SET activo = 0;
+	UPDATE BITACORACAMBIOS_ENTRADA SET activo = 0 WHERE b_identrada = @b_identrada;
 	INSERT INTO BITACORACAMBIOS_ENTRADA VALUES (@idusuarioquemodifica, @fechadecambio, @activo, @b_identrada, @b_idusuario, @b_idfuncion, @b_idsala, @b_butaca, @b_precio, @b_fechadereserva, @b_estado, @b_digitohorizontal);
 END
 GO
 
 CREATE PROC BITACORA_CAMBIOS_ENTRADA_MODIFICAR
-@fechadecambio DateTime, @activo bit
+@fechadecambio DateTime, @activo bit, @b_identrada int
 AS
 BEGIN
-	--Setear todos los activos en 0
-	UPDATE BITACORACAMBIOS_ENTRADA SET activo = 0;
-
-	--Setear solo el activo en 1
-	UPDATE BITACORACAMBIOS_ENTRADA SET 
-		activo = @activo
-	WHERE fechadecambio = @fechadecambio;
+	UPDATE BITACORACAMBIOS_ENTRADA SET activo = 0 WHERE b_identrada = @b_identrada;
+	UPDATE BITACORACAMBIOS_ENTRADA SET activo = @activo	WHERE fechadecambio = @fechadecambio;
 END
 GO
 
