d79c60afe6bfdb22d08d3a9ca715bb264e7f47d7
Lautaro Rojas <lautaro.rojas02@gmail.com>
2024-11-22 12:22:30 -0300

    Saco todas las referencias de System.Text.Json

--- BLL/BLL_CONFIGURACION.cs
+++ BLL/BLL_CONFIGURACION.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
+//using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
